95b8fc65f4a6ed2a9c2e854513db3e303b3a7d4f
fictionking <fictionking@hotmail.com>
2025-10-11 09:52:51 +0800

    换个备注图标

--- templates/components/WorkflowDesign.vue
+++ templates/components/WorkflowDesign.vue
@@ -91,7 +91,14 @@
                     </template>
                     <el-button-group size="small" class="tools-row">
                         <el-tooltip content="备注" placement="top">
-                            <el-button icon="Tickets" @click="addNode('note')" round />
+                            <el-button @click="addNode('note')" round>
+                                <template #icon>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                        <path
+                                            d="M240 432L64 432c-8.8 0-16-7.2-16-16L48 96c0-8.8 7.2-16 16-16l320 0c8.8 0 16 7.2 16 16l0 176-88 0c-39.8 0-72 32.2-72 72l0 88zM380.1 320L288 412.1 288 344c0-13.3 10.7-24 24-24l68.1 0zM0 416c0 35.3 28.7 64 64 64l197.5 0c17 0 33.3-6.7 45.3-18.7L429.3 338.7c12-12 18.7-28.3 18.7-45.3L448 96c0-35.3-28.7-64-64-64L64 32C28.7 32 0 60.7 0 96L0 416z" />
+                                    </svg>
+                                </template>
+                            </el-button>
                         </el-tooltip>
                     </el-button-group>
                     <el-menu mode="vertical" collapse class="node-menu">
